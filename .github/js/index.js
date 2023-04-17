fa31f6290e08466b6b2544737ef88640cc2c3642
github-sebastien-gineste <sebastien.gineste@teads.tv>
2023-04-17 16:58:35 +0200

    vfdvfds

--- .github/js/index.js
+++ .github/js/index.js
@@ -1,12 +1,7 @@
 module.exports = async ({github, context, core})=> {
-    const { Octokit } = require("@octokit/rest");
     const message = "coucou";
 
-    const octokit = new Octokit({
-        auth: process.env.GITHUB_TOKEN,
-    });
-
-    await octokit.rest.issues.createComment({
+    await github.rest.issues.createComment({
         issue_number: context.issue.number,
         owner: context.repo.owner,
         repo: context.repo.repo,
