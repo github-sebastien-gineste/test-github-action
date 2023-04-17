5e41f33899524124bd5fa5ec5d6de2dedef55673
github-sebastien-gineste <sebastien.gineste@teads.tv>
2023-04-17 16:18:26 +0200

    dedez

--- .github/js/index.js
+++ .github/js/index.js
@@ -1,10 +1,12 @@
-const { owner, repo } = context.repo;
-const number = context.payload.pull_request.number;
-const message = "coucou";
-const octokit = new github.GitHub(process.env.GITHUB_TOKEN);
-await octokit.issues.createComment({
-    owner,
-    repo,
-    issue_number: number,
-    body: message
-});
+module.exports = async ({github, context, core})=> {
+    const { owner, repo } = context.repo;
+    const number = context.payload.pull_request.number;
+    const message = "coucou";
+    const octokit = new github.GitHub(process.env.GITHUB_TOKEN);
+    await octokit.issues.createComment({
+        owner,
+        repo,
+        issue_number: number,
+        body: message
+    });
+}
