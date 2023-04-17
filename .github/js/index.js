1d4422b1e7bf8211c7c32b91bc9cfc2d55e1fea2
github-sebastien-gineste <sebastien.gineste@teads.tv>
2023-04-17 16:07:47 +0200

    ere

---
+++ .github/js/index.js
@@ -0,0 +1,10 @@
+const { owner, repo } = context.repo;
+const number = context.payload.pull_request.number;
+const message = "coucou";
+const octokit = new github.GitHub(process.env.GITHUB_TOKEN);
+await octokit.issues.createComment({
+    owner,
+    repo,
+    issue_number: number,
+    body: message
+});
