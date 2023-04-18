84f1ab50807f92633b5eabf24808ba7769c2b6ba
github-sebastien-gineste <sebastien.gineste@teads.tv>
2023-04-18 10:45:03 +0200

    dezdez

--- .github/go/hello.go
+++ .github/go/hello.go
@@ -2,13 +2,43 @@ package main
 
 import (
 	"fmt"
+	"os"
+
+    "github.com/google/go-github/github"
+    "golang.org/x/oauth2"
 )
 
 func main() {
-	myInput := "test"
+	// Récupérer le token d'authentification depuis les secrets de Github
+	token := os.Getenv("GITHUB_TOKEN")
 
-	output := fmt.Sprintf("Hello %s", myInput)
+	// Créer un client Github avec l'authentification
+	ts := oauth2.StaticTokenSource(
+        &oauth2.Token{AccessToken: token},
+    )
+    tc := oauth2.NewClient(oauth2.NoContext, ts)
+    client := github.NewClient(tc)
 
-	fmt.Println(fmt.Sprintf(`"{myOutput}={%s}" >> $GITHUB_OUTPUT`, output))
+	 // Récupérer les informations de la Pull Request
+	 prNumber := os.Getenv("PR_NUMBER")
+	 owner := os.Getenv("OWNER")
+	 repo := os.Getenv("REPO")
+	 pr, _, err := client.PullRequests.Get(context.Background(), owner, repo, prNumber)
+	 if err != nil {
+		 fmt.Println(err)
+		 return
+	 }
+
+	// Ajouter un commentaire à la Pull Request
+	comment := &github.IssueComment{
+		Body: github.String("Coucou"),
+    }
+    _, _, err = client.Issues.CreateComment(context.Background(), owner, repo, pr.GetNumber(), comment)
+    if err != nil {
+        fmt.Println(err)
+        return
+    }
+
+	fmt.Println("Commentaire ajouté avec succès")
 }
 
