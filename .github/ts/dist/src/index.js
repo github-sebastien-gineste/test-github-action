a607dd456356ee66eebb117592431f745c83312b
github-sebastien-gineste <sebastien.gineste@teads.tv>
2023-04-19 16:59:33 +0200

    test

--- .github/ts/dist/src/index.js
+++ .github/ts/dist/src/index.js
@@ -1,4 +1,14 @@
-module.exports = async ({github, context, core})=> {  
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// --------  diff --------
+const github_1 = require("@actions/github");
+const githubToken = process.env.GITHUB_TOKEN;
+const github = (0, github_1.getOctokit)(githubToken);
+const context = github_1.context;
+github_action();
+// --------  diff --------
+async function github_action() {
+    // ----- Same -----  
     const message = "coucou from TS";
     await github.rest.issues.createComment({
         owner: context.repo.owner,
@@ -6,4 +16,22 @@ module.exports = async ({github, context, core})=> {
         issue_number: context.issue.number,
         body: message
     });
+    const files_names = await getDiff();
+    files_names.map((file) => {
+        console.log(file.filename);
+    });
+    // ----- Same -----  
+}
+async function getDiff() {
+    if (githubToken && context.payload.pull_request) {
+        const result = await github.rest.repos.compareCommits({
+            owner: context.repo.owner,
+            repo: context.repo.repo,
+            base: context.payload.pull_request.base.sha,
+            head: context.payload.pull_request.head.sha,
+            per_page: 100
+        });
+        return result.data.files || [];
     }
+    return [];
+}
