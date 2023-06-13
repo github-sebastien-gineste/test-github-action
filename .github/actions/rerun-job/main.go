d3e1e20cb54775f404ac4780f7443f54fba10ca2
github-sebastien-gineste <sebastien.gineste@teads.tv>
2023-06-13 10:15:06 +0200

    dezsq

--- .github/actions/rerun-job/main.go
+++ .github/actions/rerun-job/main.go
@@ -2,19 +2,14 @@ package main
 
 import (
 	"actions/commons/github"
-	"os"
 )
-
-const JOB_TO_RERUN = "JOB_TO_RERUN"
 
 func main() {
 	client, ctx := github.ConnectClient()
 
 	prData := github.GetPullRequestData(client, ctx)
 
-	jobName := os.Getenv(JOB_TO_RERUN)
-
-	err := github.ReRunJobByJobName(client, ctx, prData, jobName)
+	err := github.ReRunJob(client, ctx, prData)
 	if err != nil {
 		panic(err)
 	}
